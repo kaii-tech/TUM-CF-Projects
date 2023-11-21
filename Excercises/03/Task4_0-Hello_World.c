cb7e066c4dc4bb34b31cee36412a0f247e20594c
Kaii M. Contractor <kaii@fiestagoa.com>
2023-11-21 14:57:54 +0100

    Task 4.0/4.1

---
+++ Excercises/03/Task4_0-Hello_World.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    /* Possible Implimentation 1 (C)*/
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ Excercises/03/Task4_1-Array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    /* Define an Array called "Array" with 6 integers, and assiging values to its elemets*/
+    int Array[6] = {16,17,15,47,-3,0};
+
+    int i; /* Define integer variable to use in loop*/
+    /* Define for loop that assigns "0" to "i", then loops until i reaches 5 (excluding 5 / i=0,i=1,i=2,i=3,i=4), incrementing i each run */
+    for (i = 0; i < 5; i++) 
+        /* Print all Array elements, excluding the last, with ", " afer each element */
+        printf("%d, ",Array[i]); 
+    printf("%d",Array[5]); /* Print the last Array element */
+
+    return 0;
+}
