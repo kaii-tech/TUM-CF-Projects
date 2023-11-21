f198734815c7b54a0d5ec6746b31f482db786984
Kaii M. Contractor <kaii@fiestagoa.com>
2023-11-21 16:33:48 +0100

    Task 4.2

--- Excercises/03/Task4_1-Array.c
+++ Excercises/03/Task4_1-Array.c
@@ -2,7 +2,7 @@
 
 int main() {
     /* Define an Array called "Array" with 6 integers, and assiging values to its elemets*/
-    int Array[6] = {16,17,15,47,-3,0};
+    int Array[6] = {16,7,15,47,-3,0};
 
     int i; /* Define integer variable to use in loop*/
     /* Define for loop that assigns "0" to "i", then loops until i reaches 5 (excluding 5 / i=0,i=1,i=2,i=3,i=4), incrementing i each run */
--- Excercises/03/Task4_2-Array_Statistics.c
+++ Excercises/03/Task4_2-Array_Statistics.c
@@ -2,7 +2,8 @@
 
 int main() {
     /* Array Definetion COPIED from T4.1 */
-    int Array[6] = {16,17,15,47,-3,0};
+    int Array[6] = {16,7,15,47,-3,0};
+
 
     // --------- Find Array Maximum ----------
     //Initialise "max" to first Array element
@@ -15,6 +16,7 @@ int main() {
             max = Array[i];
     }
 
+
     // --------- Find Array Minimum ----------
     //Initialise "min" to first Array element
     int min = Array[0];
@@ -26,6 +28,7 @@ int main() {
             min = Array[i];
     }
 
+
     // --------- Find Array SUM ----------
     //Initialise "sum" to 0
     int sum = 0;
@@ -36,35 +39,38 @@ int main() {
         sum += Array[i];
     }
 
+
     // --------- Find Array Median ----------
-    //Initialise "median" to 0 & Sorted Array
-    int median = 0;
-    int sortedArray[6];  
-
-    // Use and Bubble Sort (example given by Prof, in comments bellow) to sort the Array 
-    //Create a FOR loop that runs from 0 to 4 with index "i"
+    
+    // Use and Bubble Sort (example given by Prof) to sort the Array. Explain: https://www.programiz.com/dsa/bubble-sort
+    // Create a FOR loop that runs from 0 to 4 (excluding the last element) with index "i"
     for (int i = 0; i < 5; i++) {
-        //Initialise the Key / Hand to the Array Element with index "i"
-        int key = array[i];
-        int j = i - 1;
-    }
-
-
-
-
-    return 0;
-}
-
-
-/*  Bubble Sort Example from Prof!!
-
-    for (int i = 0; i < 5; ++i) {
+        // Create a loop initialises j to 1 + the current index "i", then increments it and runs until the end of the Array
         for (int j = i + 1; j < 6; ++j) {
-            if (intArray[i] > intArray[j]) {
-                int temp = intArray[i];
-                intArray[i] = intArray[j];
-                intArray[j] = temp;
+            if (Array[i] > Array[j]) {
+                int temp = Array[i];
+                Array[i] = Array[j];
+                Array[j] = temp;
             }
         }
     }
-*/
+    // Now the Array is sorted!
+
+    //Since the array has 6 elements, the Median is the mean of Array[i] for i=2 & i=3
+    int median = (Array[2] + Array[3]) / 2;
+
+
+    // --------- Find Array Mean ----------
+    
+    // Initialise and calculate 'mean' using 'sum'. Devide by 6.0 not 6 to trigger it to use the float/double datatype!!
+    double mean = (sum / 6.0); 
+
+    // Print all the values  (%lf is for double, %d is for int. Table for all datatypes: https://www.log2base2.com/C/basic/printing-value-of-a-variable.html)
+    printf("%d, ",max);
+    printf("%d, ",min);
+    printf("%d, ",sum);
+    printf("%d, ",median);
+    printf("%lf",mean);
+
+    return 0;
+}
