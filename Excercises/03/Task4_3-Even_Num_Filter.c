e6e9773acb8562ee57285016c38aaab00d8da348
Kaii M. Contractor <kaii@fiestagoa.com>
2023-11-21 19:18:42 +0100

    T4.3

---
+++ Excercises/03/Task4_3-Even_Num_Filter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    /* Array Definetion COPIED from T4.1 */
+    int Array[6] = {16,7,15,47,-3,0};
+
+    //Loop through all array elements (directly initializing i)
+    for (int i = 0; i < 6; i++) {
+        //Check if the Modulo (%) of the array element is = 0 (Even)
+        if (Array[i] % 2 == 0)
+            //If True (evan): define 
+            printf("%d ", Array[i]); //Print the value 
+    }
+
+    return 0;
+}
