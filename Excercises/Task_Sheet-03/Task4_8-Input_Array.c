810cc83bb6faa3618fd8ec9861f8380341e1c8a5
Kaii M. Contractor <kaii@fiestagoa.com>
2023-11-25 00:13:52 +0100

    Add: T4.8

---
+++ Excercises/Task_Sheet-03/Task4_8-Input_Array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    //Initilize an integer array with 5 elements
+    int Array[5];
+
+    printf("Enter an integer: ");
+    /* input Arry with 5 elements --- 
+    "%d %d" is used for the input format, this signifies an integer then a space and then an integer. 
+    the & symbol informs that the induvidual integer (%d) where to be stored, for each %d respectivly. */
+    scanf("%d %d %d %d %d", &Array[0], &Array[1], &Array[2], &Array[3], &Array[4]);
+
+    //Print the array
+    printf("%d %d %d %d %d\n", Array[0], Array[1], Array[2], Array[3], Array[4]);
+}
