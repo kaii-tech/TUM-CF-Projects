1072d5121ab19a3231af073fc72a86f8c3dac858
kaii-tech <63472188+kaii-tech@users.noreply.github.com>
2023-11-24 19:23:37 +0100

    Edit: T4.7 - More understandable

--- Excercises/Task_Sheet-03/Task4_7-Prime_Numbers.c
+++ Excercises/Task_Sheet-03/Task4_7-Prime_Numbers.c
@@ -6,30 +6,30 @@ int main() {
 
     //Loop through all array elements (directly initializing i)
     for (int i = 0; i < 6; i++) {
-        int factor = 0; //Initilises false (0) to variable factor (no factors found)
+        int prime = 1; //Initilises true (1) to variable prime (It is a prime by default and needs to be disproven)
         int divisor = 2; //Initilize the Divisor to 2 (not 0 or 1, otherwise all numbers will have a factor)
 
         //Check if the Array element is less than or equal to 0 (not prime)
         if(Array[i] <= 0){
             //number is <=0
-            factor = 1;
+            prime = 0;
         } else {
             //Check if Array element is greater than 3 (1,2,3 are Prime)
             if(Array[i] > 3) {
-                // While no factors are found and the divisor is less than half the value of the array element:
-                while(factor == 0 && divisor < Array[i]/2) {
+                // While the number is still assumed to be prime and the divisor is less than half the value of the array element:
+                while(prime == 1 && divisor < Array[i]/2) {
                     //check if the Array element has the factor "divisor"; the mod of Array[i] and divisor should be = 0
                     if(Array[i] % divisor == 0) {
-                        // Set factor to true (a factor has been found)
-                        factor = 1;
+                        // Set prime to false (a factor has been found)
+                        factor = 0;
                     } 
                     divisor++; //increment the divisor
                 }
             } 
         }
 
-        // If no factors are found (number is Prime) then print it.
-        if(factor == 0) {
+        // If prime == 1 then print it.
+        if(prime == 1) {
             printf("%d ", Array[i]);
         }
     }
