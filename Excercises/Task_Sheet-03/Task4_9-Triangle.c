72a2b8ba961c677897e98cc45db5d8798d960d88
Kaii M. Contractor <kaii@fiestagoa.com>
2023-11-26 04:11:29 +0100

    Add: 4.9

--- Excercises/Task_Sheet-03/Task4_9-Triangle.c
+++ Excercises/Task_Sheet-03/Task4_9-Triangle.c
@@ -1,3 +1,7 @@
+
+//---------- NOTE: Not quite sure what the question is... ---------------
+
+
 #include <stdio.h>
 
 main() {
@@ -7,5 +11,12 @@ main() {
     printf("Enter an Array with 3 integer elements: ");
     scanf("%d %d %d", &Array[0], &Array[1], &Array[2]);
 
+    // Check if the first element is equal to the third element and if the secound element is not equal to first (and by implication the third)
+    if(Array[0] == Array[2] && Array[0] != Array[1]) {
+        printf("yes");
+    } else {
+        printf("no");
+    }
     
+
 }
